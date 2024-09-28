72c25d2a451ef840a3038d74d82314e658749e3c
TheRealVins <urrutiadivino@gmail.com>
2024-09-28 23:33:29 +0800

    Title cased

--- SecondMiniProject.py
+++ SecondMiniProject.py
@@ -2,17 +2,17 @@ import random as r
 
 def guessthenumber(x,randomint):
     if x < randomint:
-        return("Too low")
+        return("Too low!")
     elif x > randomint:
-        return("Too high")  
+        return("Too high!")  
 
-x=int(input("enter a number: "))
-randomint=r.randint(1,10)
+x=int(input("Enter a number: "))
+randomint=r.randint(1,100)
 while x!=randomint:
     print(guessthenumber(x,randomint))
-    x=int(input("enter a number: "))
+    x=int(input("Wrong! Guess again: "))
 else:
-    print("You have guessed the number!")
+    print("You have guessed the number! Congratulations!")
 
 
     
