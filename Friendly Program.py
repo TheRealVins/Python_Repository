d0e57bca08f2a6a775682d8ada79ae53baa9d4d1
TheRealVins <urrutiadivino@gmail.com>
2024-09-28 21:57:51 +0800

    optimizedv.2

--- Friendly Program.py
+++ Friendly Program.py
@@ -1,9 +1,10 @@
 def Friendly_Program():
-    print("Hi there! I'm Siri.")
+    print("Hi there! I’m Siri.")
     name=str(input("What is your name? "))
     print(f"Hello {name}!")
     birth=int(input("What is your birthyear? "))
-    print(f"Hello! You are {2024-birth} years old.")
+    age=2024-birth
+    print(f"Hello {name}!, You are {age} years old.")
     print("What do you call a bear with no teeth?\nA gummy bear!")
     print(f"Goodbye {name}! See you next time!")
 Friendly_Program()
