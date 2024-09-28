72351a816b0d2a47f9771da1ad780369af83b018
TheRealVins <urrutiadivino@gmail.com>
2024-09-28 18:48:37 +0800

    age added

--- Java.java
+++ Java.java
@@ -1,6 +1,8 @@
 public class Java {
     public static void main(String[] args){
         String name="Vins";
+        int age=16;
         System.out.println(name);
+        System.out.println(age);
         }
 }
