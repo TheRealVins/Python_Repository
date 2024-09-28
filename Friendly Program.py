d5335eae36d44464f203693279e9405628cedf76
TheRealVins <urrutiadivino@gmail.com>
2024-09-28 19:58:30 +0800

    9 lines optimized

--- Friendly Program.py
+++ Friendly Program.py
@@ -3,8 +3,7 @@ def Friendly_Program():
     name=str(input("What is your name? "))
     print(f"Hello {name}!")
     birth=int(input("What is your birthyear? "))
-    age=2024-birth
-    print(f"Hello! You are {age} years old.")
+    print(f"Hello! You are {2024-birth} years old.")
     print("What do you call a bear with no teeth?\nA gummy bear!")
     print(f"Goodbye {name}! See you next time!")
 Friendly_Program()
