faad6119df5be0c6c7b804f6302c24bbf63abd3d
TheRealVins <urrutiadivino@gmail.com>
2024-09-28 18:55:26 +0800

    first if statement

--- Java.java
+++ Java.java
@@ -1,8 +1,13 @@
 public class Java {
     public static void main(String[] args){
         String name="Vins";
-        int age=16;
+        int age=18;
         System.out.println(name);
         System.out.println(age);
+        if (age <= 18) {
+            System.out.println("underage");
+        } else {
+            System.out.println("Adult");
         }
+    }
 }
