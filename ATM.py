5d773bec7af6840976e6bd9ebf5dbbee69ba022f
TheRealVins <urrutiadivino@gmail.com>
2024-10-01 13:14:22 +0800

    fixed looping in yes or now confirmation bug in confirmation "no"

--- ATM.py
+++ ATM.py
@@ -1,47 +1,51 @@
+def isitnumber(pin):
+    try:
+        pin=int(pin)
+        correctornot(pin)
+    except ValueError:
+        wrongpin2=input("Wrong pin! Enter NUMERICAL characters only: ")
+        isitnumber(wrongpin2)
+    else:
+        pass
+
+def correctornot(pin):
+    if pin==1234:
+        print(options())
+        confirmation()
+    if pin!=1234:
+        wrongpin=input("Wrong pin! Enter again: ")
+        isitnumber(wrongpin)
+
 def options():
-    choices=input("""[1] - Check Balance
-[2] - Withdrawal\n[3] - Deposit\n[4] - Fund Transfer\n[5] - Transaction History\nEnter your transaction: """)
-    if choices == str(1):
+    choices=int(input("""[1] - Check Balance
+[2] - Withdrawal\n[3] - Deposit\n[4] - Fund Transfer\n[5] - Transaction History\nEnter your transaction: """))
+    if choices == 1:
         return("Your balance is: $5000")
-    elif choices == str(2):
+    elif choices == 2:
         return("w")
-    elif choices == str(3):
+    elif choices == 3:
         return("D")
-    elif choices == str(4):
+    elif choices == 4:
         return("ft")
-    elif choices == str(5):
+    elif choices == 5:
         return("TH")
         
 def confirmation():
-    another=str(input("Would you like to make another transaction?\n[y/n]: "))
+    another=input("Would you like to make another transaction?\n[y/n]: ")
     if another.lower()=="y":
-        return(options())
-    if another.lower()=="n":
-        print("Thank you for using my program!")
-        return None
+        print(options())
+        print(confirmation())
+    elif another.lower()=="n":
+        return("Thank you for using my program!")
+        # endprogram()
     else:
         print("Invalid Response! Try Again.")
         return(confirmation())
 
-def isitnumber(pin):
-    try:
-        int(pin)
-        correctornot(pin)
-    except ValueError:
-        while ValueError:
-            wrongpin2=input("Wrong pin! Enter NUMERICAL characters only: ")
-            isitnumber(pin=wrongpin2)
-        else:
-            pass
-
-def correctornot(pin):
-    if pin==1234:
-        options()
-    if pin!=1234:
-        wrongpin=input("Wrong pin! Enter again: ")
-        isitnumber(pin=wrongpin)
-
-
+def endprogram():
+    while confirmation():
+        if confirmation()== None:
+            break
 
 print("ATM MACHINE: Good Day!")
 pin=input("Enter your pin: ")
