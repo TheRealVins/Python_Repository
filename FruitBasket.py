a1669c6b32ce15420d0e4adda236e28afa42e1de
TheRealVins <urrutiadivino@gmail.com>
2024-10-01 16:10:52 +0800

    final FruitBasket

--- FruitBasket.py
+++ FruitBasket.py
@@ -1,14 +1,36 @@
-print("Catch and eat any of these fruits: ('apple','orange,'mango','guava')")
-howmany=int(input("How many fruits would you like to catch? "))
+import sys as s
 
-def fruits(howmany):
+def func(howmany):
+    global basket
     basket=[]
-    choosefruit=str(input("Choose a fruit to catch. Press A, O, M, or G. :"))
-    if choosefruit.upper == "A":
-        basket.append("Apple")
-    elif choosefruit.upper == "O":
-        basket.append("Orange")
-    elif choosefruit.upper == "M":
-        basket.append("Mango")
-    elif choosefruit.upper == "G":
-        basket.append("Guava")
+    for i in range(1,howmany+1):
+        x=str(input(f"Fruits {i} to {howmany}: "))
+        if x == "a" or x == "A":
+            basket.append("Apple")
+        elif x == "o" or x == "O":
+            basket.append("Orange")
+        elif x == "m" or x == "M":
+            basket.append("Mango")
+        elif x == "g" or x == "G":
+            basket.append("Guava")
+        else:
+            print("Invalid Fruit. Enter again.")
+            func(howmany)
+
+def eat1():
+    print("Your basket now has:",basket)
+    eat=str(input("Press E to eat a fruit. "))
+    while eat == "e" or eat == "E":
+        if len(basket)==1:
+            print("Takaw mo namam! ")
+            s.exit()
+        basket.pop(-1)
+        eat1()
+
+print("Catch and eat any of these fruits: ('apple','orange','mango','guava')")
+howmany=int(input("How many fruits would you like to catch? "))
+print("Choose a fruit to catch. Press A, O, M, or G.")
+func(howmany)
+eat1()
+
+
