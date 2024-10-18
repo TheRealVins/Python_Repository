6556e9ac47b1138c0ab61538066e2ceca2e7007c
TheRealVins <urrutiadivino@gmail.com>
2024-10-18 17:18:38 +0800

    asd

--- scratch_1.py
+++
@@ -1,9 +0,0 @@
-import time as t
-import sys as s
-
-name="Vinsssss"
-for char in name:
-    t.sleep(0.3)
-    s.stdout.write(char)
-    s.stdout.flush()
-print(char)
--- scratch_3.py
+++
@@ -1,9 +0,0 @@
-row= int(input("How man rows?: "))
-columns= int(input("How man columns?: "))
-sign= int(input("What sign do you want?: "))
-
-for i in range(row):
-    for j in range(columns):
-        print(sign, end="")
-
-    print()
